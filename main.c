43b8fd1133a4ada87c5bfbd0e08956d757574f4a
kelvin <kshao@uvic.a>
2019-07-29 03:27:35 -0700

    kv sort

--- main.c
+++ main.c
@@ -6,8 +6,15 @@ int main() {
 	int numCounted = charCount("aaaaabbbbcccdde",buff);
 	printf("Counted: %d\n",numCounted);
 
-	for(int i = 95;i<110;i++){
-		printf("%c count: %d\n",i,buff[i]);
+	int keys[128];
+	for(int i = 0; i<128; i++){
+		keys[i] = i;
+	}
+
+	sort(keys,buff,128);
+
+	for(int i = 0;i<128;i++){
+		printf("%c count: %d\n",keys[i],buff[i]);
 	}
 	return 0;
 }
