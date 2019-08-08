d3c35daa16ef926ec46aea65b36afd0ba828be41
k <k>
2019-08-08 01:27:24 -0700

    Compile all the executable versions and add test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "encoder test"
+time ./cmake-build-debug/hufe0 < text/in_CAPS.txt > /dev/null
+time ./cmake-build-debug/hufe1 < text/in_CAPS.txt > /dev/null
+time ./cmake-build-debug/hufe2 < text/in_CAPS.txt > /dev/null
+echo "decoder test"
+time ./cmake-build-debug/hufd0 < text/enc_with_table.txt > /dev/null
+time ./cmake-build-debug/hufd1 < text/enc.txt > /dev/null
+time ./cmake-build-debug/hufd2 < text/enc.txt > /dev/null
