57bf1ff6349e43020cefd98fbf47c4b9405f19c5
k <k>
2019-08-08 01:01:21 -0700

    update the asm

--- hufd2.asm
+++ hufd2.asm
@@ -1,6 +1,4 @@
-.LC1:
-        .ascii  "file open failed\000"
-.LC0:
+cache6:
         .byte   84
         .word   3
         .byte   84
@@ -131,27 +129,35 @@ main:
         stmfd   sp!, {fp, lr}
         add     fp, sp, #4
         sub     sp, sp, #4980736
-        sub     sp, sp, #19712
-        sub     sp, sp, #80
-        ldr     r3, .L19
+        sub     sp, sp, #19200
+        sub     sp, sp, #88
+        ldr     r3, .L20
         ldr     r3, [r3]
         str     r3, [fp, #-20]
         ldr     r3, [fp, #-20]
         cmp     r3, #0
         bne     .L2
-        ldr     r0, .L19+4
-        bl      puts
         mov     r3, #1
-        b       .L17
+        b       .L18
 .L2:
-        ldr     r3, .L19+8
+        ldr     r3, .L20+4
         sub     r2, fp, #4
         add     r3, r2, r3
         str     r3, [fp, #-8]
         mov     r3, #0
         str     r3, [fp, #-12]
-        b       .L4
 .L6:
+        ldr     r0, [fp, #-20]
+        bl      fgetc
+        mov     r3, r0
+        str     r3, [fp, #-24]
+        ldr     r3, [fp, #-24]
+        cmn     r3, #1
+        movne   r3, #1
+        moveq   r3, #0
+        and     r3, r3, #255
+        cmp     r3, #0
+        beq     .L4
         ldr     r3, [fp, #-8]
         add     r2, r3, #1
         str     r2, [fp, #-8]
@@ -164,33 +170,25 @@ main:
         add     r3, r3, #1
         str     r3, [fp, #-12]
         ldr     r3, [fp, #-12]
-        ldr     r2, .L19+12
+        ldr     r2, .L20+8
         cmp     r3, r2
-        bgt     .L18
-.L4:
-        ldr     r0, [fp, #-20]
-        bl      fgetc
-        str     r0, [fp, #-24]
-        ldr     r3, [fp, #-24]
-        cmn     r3, #1
-        bne     .L6
-        b       .L5
-.L18:
+        movgt   r3, #1
+        movle   r3, #0
+        and     r3, r3, #255
+        cmp     r3, #0
+        bne     .L19
+        b       .L6
+.L19:
         mov     r0, r0    @ nop
-.L5:
-        sub     r3, fp, #4980736
-        sub     r3, r3, #4
-        sub     r3, r3, #16384
-        ldr     r2, .L19+16
-        sub     r3, r3, #3408
-        mov     r1, r2
-        mov     r2, #504
-        mov     r0, r3
-        bl      memcpy
+.L4:
         mov     r3, #0
         str     r3, [fp, #-16]
-        b       .L7
-.L16:
+.L17:
+        ldr     r3, [fp, #-12]
+        sub     r2, r3, #11
+        ldr     r3, [fp, #-16]
+        cmp     r2, r3
+        ble     .L7
         sub     r3, fp, #4980736
         sub     r3, r3, #4
         sub     r3, r3, #16384
@@ -205,7 +203,7 @@ main:
         cmp     r3, #1
         bls     .L8
         mov     r3, #1
-        b       .L17
+        b       .L18
 .L8:
         ldrb    r3, [fp, #-25]
         mov     r3, r3, asl #1
@@ -272,42 +270,29 @@ main:
         ldrb    r3, [fp, #-25]
         orr     r3, r2, r3
         strb    r3, [fp, #-25]
-        ldrb    r2, [fp, #-25]  @ zero_extendqisi2
-        sub     r3, fp, #4980736
-        sub     r3, r3, #4
-        sub     r3, r3, #16384
-        mov     r1, r3
-        mov     r3, r2, asl #3
-        add     r3, r1, r3
-        ldrb    r3, [r3, #-3408]        @ zero_extendqisi2
+        ldrb    r3, [fp, #-25]  @ zero_extendqisi2
+        ldr     r2, .L20+12
+        ldrb    r3, [r2, r3, asl #3]    @ zero_extendqisi2
         cmp     r3, #0
         beq     .L9
-        ldrb    r2, [fp, #-25]  @ zero_extendqisi2
-        sub     r3, fp, #4980736
-        sub     r3, r3, #4
-        sub     r3, r3, #16384
-        mov     r1, r3
-        mov     r3, r2, asl #3
-        add     r3, r1, r3
-        ldrb    r3, [r3, #-3408]        @ zero_extendqisi2
+        ldrb    r3, [fp, #-25]  @ zero_extendqisi2
+        ldr     r2, .L20+12
+        ldrb    r3, [r2, r3, asl #3]    @ zero_extendqisi2
         mov     r2, r3
-        ldr     r3, .L19+20
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, r2
         bl      _IO_putc
-        ldrb    r2, [fp, #-25]  @ zero_extendqisi2
-        sub     r3, fp, #4980736
-        sub     r3, r3, #4
-        sub     r3, r3, #16384
-        mov     r1, r3
-        mov     r3, r2, asl #3
-        add     r3, r1, r3
-        ldr     r3, [r3, #-3404]
+        ldrb    r3, [fp, #-25]  @ zero_extendqisi2
+        ldr     r2, .L20+12
+        mov     r3, r3, asl #3
+        add     r3, r2, r3
+        ldr     r3, [r3, #4]
         ldr     r2, [fp, #-16]
         add     r3, r2, r3
         str     r3, [fp, #-16]
-        b       .L7
+        b       .L17
 .L9:
         ldr     r3, [fp, #-16]
         add     r2, r3, #5
@@ -317,8 +302,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        bne     .L10
-        ldr     r3, .L19+20
+        bne     .L11
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #67
@@ -326,8 +311,8 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #6
         str     r3, [fp, #-16]
-        b       .L7
-.L10:
+        b       .L17
+.L11:
         ldr     r3, [fp, #-16]
         add     r2, r3, #6
         sub     r3, fp, #4980736
@@ -336,8 +321,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        bne     .L11
-        ldr     r3, .L19+20
+        bne     .L12
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #75
@@ -345,8 +330,8 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #7
         str     r3, [fp, #-16]
-        b       .L7
-.L11:
+        b       .L17
+.L12:
         ldr     r3, [fp, #-16]
         add     r2, r3, #7
         sub     r3, fp, #4980736
@@ -355,8 +340,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        beq     .L12
-        ldr     r3, .L19+20
+        beq     .L13
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #86
@@ -364,8 +349,8 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #8
         str     r3, [fp, #-16]
-        b       .L7
-.L12:
+        b       .L17
+.L13:
         ldr     r3, [fp, #-16]
         add     r2, r3, #8
         sub     r3, fp, #4980736
@@ -374,8 +359,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        bne     .L13
-        ldr     r3, .L19+20
+        bne     .L14
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #88
@@ -383,8 +368,8 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #9
         str     r3, [fp, #-16]
-        b       .L7
-.L13:
+        b       .L17
+.L14:
         ldr     r3, [fp, #-16]
         add     r2, r3, #9
         sub     r3, fp, #4980736
@@ -393,8 +378,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        bne     .L14
-        ldr     r3, .L19+20
+        bne     .L15
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #81
@@ -402,8 +387,8 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #10
         str     r3, [fp, #-16]
-        b       .L7
-.L14:
+        b       .L17
+.L15:
         ldr     r3, [fp, #-16]
         add     r2, r3, #10
         sub     r3, fp, #4980736
@@ -412,8 +397,8 @@ main:
         add     r3, r3, r2
         ldrb    r3, [r3, #-2904]        @ zero_extendqisi2
         cmp     r3, #0
-        beq     .L15
-        ldr     r3, .L19+20
+        beq     .L16
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #74
@@ -421,9 +406,9 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #11
         str     r3, [fp, #-16]
-        b       .L7
-.L15:
-        ldr     r3, .L19+20
+        b       .L17
+.L16:
+        ldr     r3, .L20+16
         ldr     r3, [r3]
         mov     r1, r3
         mov     r0, #90
@@ -431,22 +416,17 @@ main:
         ldr     r3, [fp, #-16]
         add     r3, r3, #11
         str     r3, [fp, #-16]
+        b       .L17
 .L7:
-        ldr     r3, [fp, #-12]
-        sub     r2, r3, #11
-        ldr     r3, [fp, #-16]
-        cmp     r2, r3
-        bgt     .L16
         mov     r3, #0
-.L17:
+.L18:
         mov     r0, r3
         sub     sp, fp, #4
         ldmfd   sp!, {fp, lr}
         bx      lr
-.L19:
+.L20:
         .word   stdin
-        .word   .LC1
         .word   -5000024
         .word   4999998
-        .word   .LC0
+        .word   cache6
         .word   stdout
--- huff_dec_2_cache.c
+++ huff_dec_2_cache.c
@@ -1,110 +1,108 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "string.h"
 
 #define  INPUT_SIZE 5000000
 #define VERBOSE 0
 
+struct CacheObj{
+    char c;
+    int actualBits;
+};
+//this can be addressed as a 6 bit number (char or int)
+const struct CacheObj cache6[] ={
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'T',3,//000xxx
+        'D',4,//0010xx
+        'D',4,//0010xx
+        'D',4,//0010xx
+        'D',4,//0010xx
+        'F',5,//00110x
+        'F',5,//00110x
+        'M',5,//00111x
+        'M',5,//00111x
+        'W',5,//01000x
+        'W',5,//01000x
+        'B',6,//010010
+        'P',6,//010011
+        'R',4,//0101xx
+        'R',4,//0101xx
+        'R',4,//0101xx
+        'R',4,//0101xx
+        'I',4,//0110xx
+        'I',4,//0110xx
+        'I',4,//0110xx
+        'I',4,//0110xx
+        'S',4,//0111xx
+        'S',4,//0111xx
+        'S',4,//0111xx
+        'S',4,//0111xx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'E',3,//100xxx
+        'H',4,//1010xx
+        'H',4,//1010xx
+        'H',4,//1010xx
+        'H',4,//1010xx
+        'U',5,//10110x
+        'U',5,//10110x
+        'G',6,//101110
+        'Y',6,//101111
+        'N',4,//1100xx
+        'N',4,//1100xx
+        'N',4,//1100xx
+        'N',4,//1100xx
+        'O',4,//1101xx
+        'O',4,//1101xx
+        'O',4,//1101xx
+        'O',4,//1101xx
+        'A',4,//1110xx
+        'A',4,//1110xx
+        'A',4,//1110xx
+        'A',4,//1110xx
+        'L',5,//11110x
+        'L',5,//11110x
+        0,5//11111x
+};
+
 int main() {
 //	FILE* f = fopen("../text/enc.txt","r");
-	FILE* f = stdin;
-	if(!f){
-		return 1;
-	}
-	int c;
+    FILE* f = stdin;
+    if(!f){
+        return 1;
+    }
+    int c;
 
-	//parse encoded input
-	char input[INPUT_SIZE];
-	char* inputIndex = input;
-	int inputSize = 0;
-	while ((c = fgetc(f))!=EOF){
-	    //subtract 48 to change '0' to 0
-		*inputIndex++ = (char)c-48;
-		if(++inputSize > INPUT_SIZE-2) break;
-	}
+    //parse encoded input
+    char input[INPUT_SIZE];
+    char* inputIndex = input;
+    int inputSize = 0;
+    while ((c = fgetc(f))!=EOF){
+        //subtract 48 to change '0' to 0
+        *inputIndex++ = (char)c-48;
+        if(++inputSize > INPUT_SIZE-2) break;
+    }
 
-	struct CacheObj{
-	    char c;
-	    int actualBits;
-	};
-	//this can be addressed as a 6 bit number (char or int)
-    struct CacheObj cache6[] ={
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'T',3,//000xxx
-            'D',4,//0010xx
-            'D',4,//0010xx
-            'D',4,//0010xx
-            'D',4,//0010xx
-            'F',5,//00110x
-            'F',5,//00110x
-            'M',5,//00111x
-            'M',5,//00111x
-            'W',5,//01000x
-            'W',5,//01000x
-            'B',6,//010010
-            'P',6,//010011
-            'R',4,//0101xx
-            'R',4,//0101xx
-            'R',4,//0101xx
-            'R',4,//0101xx
-            'I',4,//0110xx
-            'I',4,//0110xx
-            'I',4,//0110xx
-            'I',4,//0110xx
-            'S',4,//0111xx
-            'S',4,//0111xx
-            'S',4,//0111xx
-            'S',4,//0111xx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'E',3,//100xxx
-            'H',4,//1010xx
-            'H',4,//1010xx
-            'H',4,//1010xx
-            'H',4,//1010xx
-            'U',5,//10110x
-            'U',5,//10110x
-            'G',6,//101110
-            'Y',6,//101111
-            'N',4,//1100xx
-            'N',4,//1100xx
-            'N',4,//1100xx
-            'N',4,//1100xx
-            'O',4,//1101xx
-            'O',4,//1101xx
-            'O',4,//1101xx
-            'O',4,//1101xx
-            'A',4,//1110xx
-            'A',4,//1110xx
-            'A',4,//1110xx
-            'A',4,//1110xx
-            'L',5,//11110x
-            'L',5,//11110x
-            0,5//11111x
-    };
-
-	int i = 0;
+    int i = 0;
 
     while(i<inputSize-11){
-		//note - only stores 6 bits out of 8
+        //note - only stores 6 bits out of 8
         unsigned char buffer;
-		//first bit
-		buffer = input[i];
-		if(buffer>1)return 1;
-		//shift for bits 2-6
-		buffer <<= 1;
-		buffer |= input[i+1];
+        //first bit
+        buffer = input[i];
+        if(buffer>1)return 1;
+        //shift for bits 2-6
+        buffer <<= 1;
+        buffer |= input[i+1];
         buffer <<= 1;
         buffer |= input[i+2];
         buffer <<= 1;
@@ -151,7 +149,7 @@ int main() {
                 i+=11;
             }
         }
-	}
+    }
 
-	return 0;
+    return 0;
 }
